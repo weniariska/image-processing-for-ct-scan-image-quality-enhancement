33943158020a7da027fe624815a4e9202560d477
weniariska <weniariska1@gmail.com>
2022-08-16 16:38:55 +0700

    commit project

--- meningkatkan-kualitas-citra-ctscan.py
+++ meningkatkan-kualitas-citra-ctscan.py
@@ -34,13 +34,15 @@ plt.show()
 ret,img_th = cv.threshold(img_contras,175,255,cv.THRESH_BINARY)
 
 # menampilkan citra hasil
-cv.imshow('final', img_th)
+final = cv.bitwise_and(img_contras,img_contras,mask=img_th)
+cv.imshow('Final', final)
 
 # menyimpan citra
 cv.imwrite('res_bitwise.jpg',img_bitwise)
 cv.imwrite('res_noise_removal.jpg',img_noiseRemov)
 cv.imwrite('res_contras.jpg',img_contras)
-cv.imwrite('final.jpg',img_th)
+cv.imwrite('threshold.jpg',img_th)
+cv.imwrite('final.jpg',final)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
